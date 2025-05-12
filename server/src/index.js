dc55dcf80afd8c092eeb045b06e4523d19e2534b
Billy <osayebilly98@gmail.com>
2025-05-12 11:21:17 +0300

    Set up backend with Node.js, Express, and JavaScript

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('TODO App Backend');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
